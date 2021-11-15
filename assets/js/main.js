b5c48f548e44253e721f5ce4f9b88174be1e8bf8
azerahmedov <ehmedovh111@gmail.com>
2021-11-16 01:13:46 +0400

    desktop hazirlandi

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+    const swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 0,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
